5479966381c4c9e8622300044a49d21d6e81e04b
Pierre Portejoie <portejoiepierre@gmail.com>
2013-09-24 10:35:20 +0200

    Cleans up directories
    Removes unused files
    Moves old files to old/

--- tests/EMgetstest.rb
+++
@@ -1,7 +0,0 @@
-require "eventmachine"
-
-EM.run {
-  EM.add_periodic_timer(2) {
-  puts  " reading #{gets.chomp}"
-  }
-}
--- tests/videohandler.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'raspeomix'
-
-EM.run {
-  client = Raspeomix::VideoClient.new("192.168.1.48")
-}
