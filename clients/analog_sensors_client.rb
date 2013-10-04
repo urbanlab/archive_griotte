dbd0e23f8f3928e26f3bd95d3b337fba627b978f
Michel Blanc <mb@mbnet.fr>
2013-10-04 16:55:13 +0200

    Fixes coma instead of period

--- clients/analog_sensors_client.rb
+++ clients/analog_sensors_client.rb
@@ -2,7 +2,7 @@
 
 require 'raspeomix'
 
-p = Raspeomix::Client::AnalogSensorsClient::SensorProfile.new("Maxbotix EZ-1", :conversion_formula => "x 0,003858 /", :units => "cm", :description => "Distance in cm")
+p = Raspeomix::Client::AnalogSensorsClient::SensorProfile.new("Maxbotix EZ-1", :conversion_formula => "x 0.003858 /", :units => "cm", :description => "Distance in cm")
 
 s = Raspeomix::Client::AnalogSensorsClient::Sensor.new(:an0, p, 3, :'12bits')
 a = Raspeomix::Client::AnalogSensorsClient.new(s)
