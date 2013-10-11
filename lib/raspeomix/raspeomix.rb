171973244719384f62613f66255eba5763c24ed1
Michel Blanc <mb@mbnet.fr>
2013-10-11 11:09:46 +0000

    Implements Faye logging
    Logs will be sent to Faye when RASP_LOG=FAYE

--- lib/raspeomix/raspeomix.rb
+++ lib/raspeomix/raspeomix.rb
@@ -1,4 +1,5 @@
 require 'logger'
+require 'faye'
 
 class NullObject
   def method_missing(*args, &block)
@@ -7,6 +8,32 @@ class NullObject
 end
 
 module Raspeomix
+
+  # FayeLogger class sends log to faye channel /log
+  #
+  class FayeLogger
+    include Client::FayeClient
+
+    LEVELS = { 'D' => 'debug',
+               'I' => 'info',
+               'W' => 'warn',
+               'E' => 'error',
+               'F' => 'fatal', }
+
+    # Writes logs to faye
+    #
+    # @param [String] message message to send to the Faye log
+    def write(message)
+      publish("/log/#{LEVELS[message[0]]}", { :message => message })
+    end
+
+    # Required to be a logger
+    #
+    def close
+      true
+    end
+  end
+
   def self.logger
     @logger ||= initialize_log
   end
@@ -15,12 +42,26 @@ module Raspeomix
     @logger = logger
   end
 
+  # Initilizes logger
+  #
+  # @todo check if file is passed, and activate log trimming (shift_age = 0,
+  # shift_size = 1048576)
   def self.initialize_log
+    #
+    # If RASP_LOG is not defined, we return a Null logger
     return NullObject.new unless ENV['RASP_LOG']
 
-    @do_log = true
+    # Set log to RASP_LOG environment variable
     log = ENV['RASP_LOG'] if (ENV['RASP_LOG'])
+    
+    # STDOUT logging mode
     log = STDOUT if (ENV['RASP_LOG'] == 'STDOUT')
+
+    # FAYE logging mode
+    log = FayeLogger.new if log == 'FAYE'
+
+    # RASP_LOG was defined but empty, return Null logger
+    return NullObject.new if log == ""
 
     begin
       Logger.new(log)
