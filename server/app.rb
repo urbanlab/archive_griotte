8bbbf5dcf0a833f45c849cf57d62ec7a72adea5b
Michel Blanc <mb@mbnet.fr>
2013-10-11 13:32:03 +0000

    Adds authentication for admin space
    Added auth for /admin

--- server/app.rb
+++ server/app.rb
@@ -6,9 +6,58 @@ ROOT_DIR = File.expand_path('../', __FILE__)
 set :root, ROOT_DIR
 set :logging, false
 
-get '/' do
-  File.read(ROOT_DIR + '/public/index.html')
+# Change me
+set :password, 'foobar'
+set :app_id, 'raspeomix-123456'
+set :token, (0...32).map { (97 + rand(26)).chr }.join
+
+enable :sessions
+
+# Authentication helpers
+helpers do
+  def admin? ; session[settings.app_id] == settings.token ; end
+  def protected! ; halt [ 401, 'Not Authorized' ] unless admin? ; end
 end
+
+get '/favicon.ico' do
+  halt 404
+end
+
+get '/login/' do
+  redirect '/admin/' if admin?
+  File.read(File.join(ROOT_DIR, 'public/login/index.html'))
+end
+
+get '/logout/' do
+  session.clear
+  redirect '/login/'
+end
+
+post '/login/' do
+  if params['password'] == settings.password
+    session[settings.app_id] = settings.token
+    redirect '/admin/'
+  else
+    redirect '/login/'
+  end
+end
+
+# Redirects to the same place with a / at the end
+# Note that this prevents sinatra from serving file in the top directory
+get '/:dir' do
+  redirect "#{params[:dir]}/"
+#  File.read(ROOT_DIR + "/public/#{params[:dir]}/index.html')
+end
+
+get '/admin/' do
+  protected!
+  File.read(File.join(ROOT_DIR, 'public/admin/index.html'))
+end
+
+get '/:dir/' do
+  File.read(File.join(ROOT_DIR, 'public', params[:dir], 'index.html'))
+end
+
 
 #get '/post' do
 #  env['faye.client'].publish('/mentioning/*', {
@@ -17,6 +66,7 @@ end
 #  })
 #  params[:message]
 #end
+
 Faye::Logging.log_level = :debug
 Faye.logger = lambda { |m| puts m }
 
